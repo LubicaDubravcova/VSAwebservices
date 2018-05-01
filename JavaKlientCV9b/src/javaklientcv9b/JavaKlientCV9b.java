b1e731283827c7d72696afde1a16ab3f5f7b09cd
Ľubica <l.dubravcova@gmail.com>
2018-05-01 14:57:09 +0200

    cviko9 2.+3. uloha LIST javaclient

---
+++ JavaKlientCV9b/src/javaklientcv9b/JavaKlientCV9b.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaklientcv9b;
+
+import proxy.NewJerseyClient;
+
+/**
+ *
+ * @author Dell
+ */
+public class JavaKlientCV9b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJerseyClient client = new NewJerseyClient();
+        System.out.println(client.getAll());
+        System.out.println(client.getMenu("2"));
+        client.putMenu("langos", "0");
+        client.postMenu("zmrzlina");
+        System.out.println(client.getAll());
+        client.deleteMenu("3");
+        System.out.println(client.getAll());    
+        
+       
+    }
+
+    
+}
