5175c6e966b9c7eed40f070fdae7ba0c81a902bd
Ľubica Dúbravcová <32597067+LubicaDubravcova@users.noreply.github.com>
2018-05-20 23:05:36 +0200

    fungujeeeee

--- Cviko11JAXB/src/cviko11jaxb/Cviko11JAXB.java
+++ Cviko11JAXB/src/cviko11jaxb/Cviko11JAXB.java
@@ -7,8 +7,12 @@ package cviko11jaxb;
 
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 
@@ -24,21 +28,70 @@ public class Cviko11JAXB {
     public static void main(String[] args) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(Adresar.class);
         Unmarshaller um = context.createUnmarshaller();
-        Adresar adresar = (Adresar) um.unmarshal(new File("C:\\Users\\Dell\\Desktop\\adresar.xml"));
+       // Adresar adresar = (Adresar) um.unmarshal(new File("C:\\Users\\Dell\\Desktop\\adresar.xml"));
+       Adresar adresar = (Adresar) um.unmarshal(new File("test.xml"));
         
         for (Osoba o:adresar.getAdresar())
             System.out.println(o.getMeno());
+        
+        
         System.out.println("-------------------");
         
         for (Osoba o:adresar.getAdresar()){
-            if (o.getGen()=="zena")
+            if (o.getGen().compareTo("zena")==0)
                 System.out.println(o.getMeno());
         }
-         System.out.println("-------------------"); 
+        
+        
+        System.out.println("-------------------"); 
+        
+        Set<String> distinctBydliska = new HashSet<>();
         for (Osoba o:adresar.getAdresar())
             if (o.getBydlisko()!=null)
-                System.out.println(o.getBydlisko());
+                distinctBydliska.add(o.getBydlisko());
+        for (String city:distinctBydliska)
+            System.out.println(city);
+        
+        
         System.out.println("-------------------");
+        
+        Set<String> distinctMena=new HashSet<>();
+        Set<String> duplikaty =new HashSet<>();
+        
+        for (Osoba o:adresar.getAdresar()){ 
+            if (!distinctMena.contains(o.getMeno()))
+                distinctMena.add(o.getMeno());
+            else
+                duplikaty.add(o.getMeno());
+        }
+        if (duplikaty.isEmpty())
+            System.out.println("Nie su ziadne duplikaty");
+        else{
+            System.out.println("Su duplikaty. Tieto mena/meno sa nachadza viackrat:"); 
+            for(String meno:duplikaty)
+                System.out.println(meno);
+            }
+        
+       
+        
+        //-------------------VYTVORENIE XML-----------------------
+        /*LinkedList<Osoba> adresar=new LinkedList<>();
+             
+        Osoba o=new Osoba("zena","Zuzana Kratochvilova","Bratislava");
+        Osoba o2=new Osoba("muz","Igor Vdovec","Puchov");
+        Osoba o3=new Osoba("muz","Igor Vdovec","Puchov");
+        adresar.add(o);
+        adresar.add(o2);
+        adresar.add(o3);
+        
+        Adresar a1=new Adresar();
+        a1.setAdresar(adresar);
+     
+        JAXBContext context = JAXBContext.newInstance(Adresar.class);
+        Marshaller m = context.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        m.marshal(a1, System.out);
+        m.marshal(a1, new File("test.xml"));*/
        
         
     }
